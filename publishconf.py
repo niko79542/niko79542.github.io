1a1e430cd990d21505cb5e4fa9ab307874e7f2cd
niko79542 <niko79542@yahoo.com>
2017-01-21 14:26:46 -0800

    add gun data analysis

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://niko79542.github.io'
+SITEURL = 'https://niko79542.github.io'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
