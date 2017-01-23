b498dfdd3bcabeecee45c5cb055ac89f6881c407
niko79542 <niko79542@yahoo.com>
2017-01-22 19:26:34 -0800

    switch to dev-random theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -31,38 +31,8 @@ DEFAULT_PAGINATION = 10
 
 # USE THEME
 
-THEME = "/home/niko/Documents/blog_extra/pelican-themes/blue-penguin-c5e23e7753367b5beacce87b732cd1567c4818f9"
+THEME = "/home/niko/Documents/blog_extra/dev-random/"
 
-# SEt  blue-penguin-settings
-
-# all defaults to True.
-DISPLAY_HEADER = True
-DISPLAY_FOOTER = True
-DISPLAY_HOME   = True
-DISPLAY_MENU   = True
-
-# provided as examples, they make ‘clean’ urls. used by MENU_INTERNAL_PAGES.
-TAGS_URL           = 'tags'
-TAGS_SAVE_AS       = 'tags/index.html'
-AUTHORS_URL        = 'authors'
-AUTHORS_SAVE_AS    = 'authors/index.html'
-CATEGORIES_URL     = 'categories'
-CATEGORIES_SAVE_AS = 'categories/index.html'
-ARCHIVES_URL       = 'archives'
-ARCHIVES_SAVE_AS   = 'archives/index.html'
-
-# use those if you want pelican standard pages to appear in your menu
-MENU_INTERNAL_PAGES = (
-    ('Tags', TAGS_URL, TAGS_SAVE_AS),
-    ('Authors', AUTHORS_URL, AUTHORS_SAVE_AS),
-    ('Categories', CATEGORIES_URL, CATEGORIES_SAVE_AS),
-    ('Archives', ARCHIVES_URL, ARCHIVES_SAVE_AS),
-)
-# additional menu items
-MENUITEMS = (
-    ('GitHub', 'https://github.com/'),
-    ('Linux Kernel', 'https://www.kernel.org/'),
-)
 
 
 # Uncomment following line if you want document-relative URLs when developing
